b65037834c6bafc99edab9c1cf58254c69cfe018
MyMindMyBodyMySoul <falco.bechtle@online.de>
2015-11-14 16:24:46 +0100

    Added some stuff we agreed
    Added user_queue
    added method for receiving new list
    added method for checking state of user_queue

--- queue_manager.py
+++ queue_manager.py
@@ -9,24 +9,51 @@ except ImportError:
 
 host_queue = Queue()
 result_queue = Queue()
+user_queue = Queue() #Queue will hold user requests
 
 host = "localhost"
 port = 50001
 authkey = b"abc"
 
+'''
+    TODO
+    Explore possibility of implementing the server as singleton
 
+    On having a new Queue: finish crawling current host_queue or discard it?
+'''
 class QueueServer(BaseManager):
     def __init__(self):
         self.register('host_queue', callable=lambda: host_queue)
         self.register('result_queue', callable=lambda: result_queue)
+        self.register('user_queue',callable=lambda :  user_queue)
 
         BaseManager.__init__(self, address=(host, port), authkey=authkey)
 
+    # Method for getting one url-object
+    def getURl(self):
+        if not user_queue.empty():
+            return user_queue._get()
+
+        return host_queue.get()
+
+    def empty_queue(self):
+        # two possibilities: emptying with a loop or garbage collection
+        #host_queue = Queue()
+        while(not host_queue.empty()):
+            host_queue.get()
+
+    def putNewList(self, *newList):
+        for item in newList:
+            host_queue.put(item)
+
+
+# End of QueueServer class
 
 
 class QueueClient(BaseManager):
     def __init__(self):
         self.register('host_queue')
         self.register('result_queue')
+        self.register('user_queue')
         BaseManager.__init__(self, address=(host, port), authkey=authkey)
         self.connect()
