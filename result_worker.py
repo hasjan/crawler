2ef8db17e9e91fe7b211399cc3c6d9cec8339402
lukas <lum@mailbox.org>
2015-11-19 17:00:07 +0100

    Added Database class and insert_result Method

--- result_worker.py
+++ result_worker.py
@@ -1,6 +1,32 @@
+"""
+This module ...
+"""
+
 
 from queue_manager import QueueClient
+from pymongo import MongoClient, errors
+from time import sleep
 
+
+class Database(object):
+
+    def __init__(self):
+        self.db_client = MongoClient('localhost', 27017)
+        self.db = self.db_client.tls_crawler
+        self.coll = self.db.tls_scan_results
+
+    def insert_result(self, db_input):
+        # retry the insert, if the connection to the database is lost
+        for i in range(60):
+            try:
+                self.coll.insert_one(db_input)
+                break
+            except errors.AutoReconnect as e:
+                print e
+                sleep(5)
+        else:
+            raise Exception("Could not insert!")
+        return
 
 if __name__ == "__main__":
 
@@ -10,11 +36,15 @@ if __name__ == "__main__":
     # get appropriate queue from QueueClient
     qm = c.queue_manager()
 
+    # get instance of Database
+    mdb = Database()
+
     while True:
         result = qm.next_result()
 
-        print(result.keys())
         # do something with result ...
-        print(result["target"])
-        print(result)
 
+        # mdb.insert_result({"test": "test"})
+        # print(result.keys())
+        # print(result["target"])
+        # print(result)
